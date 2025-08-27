ae52211b806737f58f2a5e7e7610e93a4bd4877e
Dylan Glynn <glynndylan36@gmail.com>
2025-08-27 09:53:30 +0100

    createdangularapp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterLink,RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/displaydetails/displaydetails.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-displaydetails',
+  imports: [RouterLink,RouterLinkActive],
+  templateUrl: './displaydetails.component.html',
+  styleUrl: './displaydetails.component.css'
+})
+export class DisplaydetailsComponent {
+
+}
